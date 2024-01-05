4a58effc565980ca8bfcd6dea593fd09230e8aeb
Muskanashi77 <redmi829244@gmail.com>
2024-01-05 13:34:54 +0530

    ok

--- program1.py
+++ program1.py
@@ -1,13 +1,21 @@
 class Solution(object):
-    def isValid(self, s):
+        def isValid(self, s):
         """
         :type s: str
         :rtype: bool
         """
+    stack = []
+    brackets = {')': '(', '}': '{', ']': '['}
+
+    for char in s:
+        if char in brackets.values():
+            stack.append(char)
+        elif char in brackets.keys():
+            if not stack or stack.pop() != brackets[char]:
+                return False
+        else:
+            return False
+
+    return not stack
         pass
     
-
-
-
-  
-
